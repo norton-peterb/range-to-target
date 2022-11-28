c00cb528390619104766a27bc14330fbd76eb597
Peter B Norton <norton.peterb@gmail.com>
2022-11-28 11:43:19 +0000

    Update to include LCD Test Sequence

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include "TM1637Display.h"
+
+#define LCD_CLK 3
+#define LCD_DIO 4
+
+TM1637Display lcd = TM1637Display(LCD_CLK,LCD_DIO);
+
+const uint8_t test_lcd[] {
+  0b01111111,0b01111111,0b01111111,0b01111111
+};
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.setBrightness(3);
+  lcd.setSegments(test_lcd,4,0);
+  delay(1000);
+  lcd.clear();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
